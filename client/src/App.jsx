c0446916a2ab10a3e0b40520887076aeaffa5a15
douglas <douglasmaxton@gmail.com>
2024-07-15 13:50:28 +0100

    fix: fetching data from server to client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,27 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  console.log("hello world");
+  const [state, setState] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/")
+      .then(async (response) => {
+        const results = await response.json();
+        setState(results.data);
+      })
+      .catch((err) => {
+        console.log("error", err);
+      });
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
+        {state
+          ? state.map((items) => <p key={items.id}>{items.title}</p>)
+          : null}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
--- server/routes/homeRoutes.mjs
+++ server/routes/homeRoutes.mjs
@@ -1,13 +1,18 @@
 import { Router } from "express";
+import { prisma } from "../lib/prisma.mjs";
 
 const router = Router();
 
 router.get("/", async (req, res) => {
+  const data = await prisma.user.findMany({});
+
+  console.log("data", data);
+
   res.status(200).json({
     message: "Welcome to the home page",
     status: "success",
     statusCode: 200,
-    data: "This is the home page no data to be fetched",
+    data,
   });
 });
 
